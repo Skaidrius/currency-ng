9fc880a9711deed01cee2f4c5ca1a6919951c08d
Skaidrius <skaidrius@gmail.com>
2017-03-06 11:45:47 +0000

    bootstrap layout changes

--- app/services/convert.js
+++ app/services/convert.js
@@ -2,9 +2,7 @@ angular.module('curRate')
     .factory('currencyConverter', function() {
       var ltlToForeignRates = {
         LTL: 1,
-        EUR: 3.4528,
-        USD: 2.80,
-        PLN: 0.78
+        EUR: 3.4528
       };
       var convert = function (amount, inCurr, outCurr) {
         return amount * ltlToForeignRates[outCurr] / ltlToForeignRates[inCurr];
