4246766ecbcb4c9ff85d967464ccd28bcff802ab
Skaidrius <skaidrius@gmail.com>
2018-06-07 09:08:56 +0000

    add BYN, CNY

--- app/services/getData.js
+++ app/services/getData.js
@@ -16,9 +16,9 @@ function getData($http){
     
     return $http
         //this code of new API with API legacy free with ssl access key
-        .get("https://data.fixer.io/api/latest?access_key=a058842594f14413cc3cb442b3bab132&symbols=CZK,GBP,HRK,NOK,PLN,USD,TRY,BYN")
+        .get("https://data.fixer.io/api/latest?access_key=a058842594f14413cc3cb442b3bab132&symbols=CZK,GBP,HRK,NOK,PLN,USD,TRY,BYN,CNY")
         // this code worked before 2018-06-01
-        // .get("https://api.fixer.io/latest?symbols=CZK,GBP,HRK,NOK,PLN,USD,TRY,BYN,CNY")
+        // .get("https://api.fixer.io/latest?symbols=CZK,GBP,HRK,NOK,PLN,USD,TRY")
         .success(function(response){
             vm.data = response;
             // console.log(vm.data);
